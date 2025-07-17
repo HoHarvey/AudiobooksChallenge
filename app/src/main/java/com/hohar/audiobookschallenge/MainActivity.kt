eb81ada0800d86596938b9738d0c061ffca451e2
HoHarvey <harvey_1997@hotmail.ca>
2025-07-17 19:23:47 -0400

    added top bar and changed theme to light theme instead of dark theme

--- app/src/main/java/com/hohar/audiobookschallenge/MainActivity.kt
+++ app/src/main/java/com/hohar/audiobookschallenge/MainActivity.kt
@@ -6,42 +6,38 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hohar.audiobookschallenge.ui.theme.AudiobooksChallengeTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AudiobooksChallengeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    topBar = {
+                        // add top bar with title of Podcasts
+                        TopAppBar(
+                            title = { Text("Podcasts") }
+                        )
+                    }
+                ) { innerPadding ->
+                    // Your main screen content goes here
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AudiobooksChallengeTheme {
-        Greeting("Android")
-    }
-}
+
